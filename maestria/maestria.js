e26a4fd22dbad52b244ebdafdc6de55b3389ed68
JoallyOtamendi <joallyotamendi@gmail.com>
2024-03-17 20:51:54 -0600

    ajuste de botones

--- maestria/maestria.js
+++ maestria/maestria.js
@@ -1,9 +1,12 @@
-function muestra__oculta(id){
+function muestra__oculta(id, btn){
     let div = document.getElementById(id);
+    let boton = document.getElementById(btn);
     if (div.style.display == "flex"){
         div.style.display = "none";
+        boton.textContent="Mostrar";
     }
     else{
         div.style.display = "flex";
+        boton.textContent="Ocultar";
     }
 }
